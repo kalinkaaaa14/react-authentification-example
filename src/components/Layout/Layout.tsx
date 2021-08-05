46f0129dbe067cd6a5478aca1dda6b1c584265f0
kalinkaaaa14 <kalinkaaaa14@gmail.com>
2021-08-05 18:57:40 +0300

    auth-example done

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+import MainNavigation from './MainNavigation';
+
+const Layout: React.FC = (props) => {
+    return (
+        <Fragment>
+            <MainNavigation />
+            <main>{props.children}</main>
+        </Fragment>
+    );
+};
+
+export default Layout;
---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,8 @@
+import UserProfile from '../components/Profile/UserProfile';
+import React from "react";
+
+const ProfilePage = () => {
+    return <UserProfile />;
+};
+
+export default ProfilePage;
